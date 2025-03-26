71e2cd98fabb86638cf38e27afeef30a92a52bc5
pulkit <pulkit.k@tatrasdata.com>
2025-03-26 16:28:41 +0530

    Added support for python 3.11

--- setup.py
+++ setup.py
@@ -1,13 +1,43 @@
-#!/usr/bin/env python
-# Copyright (c) 2014 lda developers, Vikash Singh (vi3k6i5)
-#
-# This Source Code Form is subject to the terms of the Mozilla Public
-# License, v. 2.0. If a copy of the MPL was not distributed with this file, You
-# can obtain one at http://mozilla.org/MPL/2.0/.
+from setuptools import setup, Extension, find_packages
 
-import setuptools
+# Define the C extension
+guidedlda_extension = Extension(
+    'guidedlda._guidedlda',
+    sources=[
+        'guidedlda/_guidedlda.c',
+        'guidedlda/gamma.c',
+    ]
+)
 
-setuptools.setup(
-    setup_requires=['pbr'],
-    pbr=True
+# Setup function
+setup(
+    name='guidedlda',
+    version='2.0',
+    description='Topic modeling with Guided latent Dirichlet allocation',
+    long_description=open('README.rst').read(),
+    author='Vikash Singh',
+    author_email='vikash.duliajan@gmail.com',
+    classifiers=[
+        'Intended Audience :: Information Technology',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)',
+        'Programming Language :: C',
+        'Programming Language :: Cython',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.7',
+        'Operating System :: MacOS',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: POSIX',
+        'Operating System :: Unix',
+    ],
+    packages=find_packages(),
+    install_requires=[
+        'numpy',
+    ],
+    ext_modules=[guidedlda_extension],
+    setup_requires=['setuptools>=61.0', 'wheel'],
 )
